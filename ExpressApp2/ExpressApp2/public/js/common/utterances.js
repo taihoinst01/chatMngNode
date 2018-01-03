b96fd55ed1b3c74408c55c173b0e1e43cfb25cdf
YOODOOYEON <endus0713@naver.com>
2018-01-03 16:09:35 +0900

    utterance js추가.

--- ExpressApp2/ExpressApp2/public/js/common/utterances.js
+++ ExpressApp2/ExpressApp2/public/js/common/utterances.js
@@ -91,3 +91,69 @@ function utterInput(queryText) {
 
     }); // ------      ajax 끝-----------------
 }
+
+
+//---------------두연 추가
+
+function openModalBox(target){
+    if(target === '#rename_chatbot'){
+        $('#lay').css('display','none');
+        $('#reName').val($('#currentAppName').val());
+    }
+    // 화면의 높이와 너비를 변수로 만듭니다.
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+
+    // 마스크의 높이와 너비를 화면의 높이와 너비 변수로 설정합니다.
+    $('.mask').css({'width':maskWidth,'height':maskHeight});
+
+
+    // 레이어 팝업을 가운데로 띄우기 위해 화면의 높이와 너비의 가운데 값과 스크롤 값을 더하여 변수로 만듭니다.
+    var left = ( $(window).scrollLeft() + ( $(window).width() - $('#create_dlg').width()) / 2 );
+    var top = ( $(window).scrollTop() + ( $(window).height() - $('#create_dlg').height()) / 2 );
+
+    // css 스타일을 변경합니다.
+    $(target).css({'left':left,'top':top, 'position':'absolute'});
+
+    // 레이어 팝업을 띄웁니다.
+    $(target).show();
+
+    $('html').css({'overflow': 'hidden', 'height': '100%'});
+        $('#element').on('scroll touchmove mousewheel', function(event) { // 터치무브와 마우스휠 스크롤 방지
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+    });
+    wrapWindowByMask();
+}
+
+function wrapWindowByMask(){ //화면의 높이와 너비를 구한다. 
+    var maskHeight = $(document).height(); 
+    var maskWidth = $(window).width(); //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채운다. 
+    $('#layoutBackground').css({'width':maskWidth,'height':maskHeight}); //마스크의 투명도 처리 
+    $('#layoutBackground').fadeTo("fast",0.7); 
+} 
+
+function selectInent(intent) {
+    //intent하위 entity 존재하면 entity select box disable제거되게 구현해야함
+    $('#entityList').removeAttr("disabled");
+}
+
+function selectEntity(entity) {
+    //intent하위 entity 존재하면 entity select box disable제거되게 구현해야함
+    $('#btnAddDlg').removeAttr("disabled");
+    $('#btnAddDlg').removeClass("disable");
+}
+
+function initMordal(objId, objName) {
+    //<option selected="selected" disabled="disabled">Select Intent<!-- 서비스 선택 --></option>
+    if (objId == 'entityList') {
+        $('#'+ objId).attr("disabled", "disabled");
+    }
+    $('#btnAddDlg').attr("disabled", "disabled");
+    $('#btnAddDlg').addClass("disable");
+
+    $('#'+ objId + ' option:eq(0)').remove();
+    $('#'+ objId ).prepend('<option selected="selected" disabled="disabled">' + objName + '</option>');
+
+}
