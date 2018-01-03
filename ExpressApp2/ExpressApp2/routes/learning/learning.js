4b7a98c78991ddd6672fa71985a122bf1a0a4207
YOODOOYEON <endus0713@naver.com>
2018-01-03 10:36:05 +0900

    learning-utterances html 추가

--- ExpressApp2/ExpressApp2/routes/learning/learning.js
+++ ExpressApp2/ExpressApp2/routes/learning/learning.js
@@ -27,4 +27,34 @@ router.get('/utterances', function (req, res) {
         title: 'learning utterances page'
     } );
 });
+
+/* sent utterances ajax */
+router.post('/utterInputAjax', function(req, res, next) {
+
+    //view에 있는 data 에서 던진 값을 받아서
+    var iptUtterance = req.body.iptUtterance;
+
+    //json 형식으로 보내 준다.
+    res.send({
+        result:true, 
+        iptUtterance:iptUtterance,
+        entities: [
+            {
+                'startIndex': '0',
+                'endIndex': '2',
+                'entityName': 'hi',
+            },
+            {
+                'startIndex': '4',
+                'endIndex': '6',
+                'entityName': '인사',
+            }
+        ]
+    });
+
+});
+
+
+
+
 module.exports = router;
